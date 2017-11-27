0f12ae75df8368c590b4b6971674a0e4661f5cef
Mitchell M <mitchellm1618@gmail.com>
2017-11-27 10:03:44 -0800

    readme and solution for kata

---
+++ 0-replace-all-dots/solution.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var replaceDots = function(str) { //declares replaceDots as a function
+  return str.replace(/\./g, '-'); //uses replace method to target every . globally, then replace with -
+};
